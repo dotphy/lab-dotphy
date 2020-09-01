9c7e2c4f75d761402257a839beef217adf04342e
Aaditay <aaditaygargktl@gmail.com>
2020-09-01 14:47:34 +0530

    ➕ Controls Component is functional now

--- src/Components/Controls/Controls.js
+++ src/Components/Controls/Controls.js
@@ -7,29 +7,54 @@ import graph from "../../Assets/graph.svg";
 import configure from "../../Assets/configure.svg";
 import "./controls.css";
 
-function Play({ status }) {
-  let src = status == "play" ? play : pause;
-  return <img src={src} />;
+function Play(props) {
+  let src = props.status == "play" ? play : pause;
+  return <img src={src} onClick={props.onClick} />;
 }
 
-function Repeat() {
-  return <img src={repeat} />;
+function Repeat(props) {
+  let animation = props.status;
+  return <img src={repeat} onClick={props.onClick} className={animation} />;
 }
 
+function Graph(props) {
+  let status = props.status;
+  return <img src={graph} className={status} onClick={props.onClick} />;
+}
+
+function Configure(props) {
+  let status = props.status;
+  return <img src={configure} className={status} onClick={props.onClick} />;
+}
 export function Controls(props) {
   let [playStatus, setPlayStatus] = useState("play");
+  let [repeatStatus, setRepeatStatus] = useState("reload1");
+  let [graphStatus, setGraphStatus] = useState("off");
+  let [configureStatus, setConfigureStatus] = useState("off");
 
-  function handleClick() {
-    console.log("Click Handled");
-    setPlayStatus("pause");
+  function handlePlayClick() {
+    playStatus == "play" ? setPlayStatus("pause") : setPlayStatus("play");
+  }
+  function handleRepeatClick() {
+    repeatStatus == "reload1"
+      ? setRepeatStatus("reload2")
+      : setRepeatStatus("reload1");
+  }
+  function handleGraphStatus() {
+    graphStatus == "off" ? setGraphStatus("on") : setGraphStatus("off");
+  }
+  function handleConfigureStatus() {
+    configureStatus == "off"
+      ? setConfigureStatus("on")
+      : setConfigureStatus("off");
   }
 
   return (
     <div className={`controls ${props.className}`}>
-      <Play status={playStatus} onClick={handleClick} />
-      <Repeat />
-      <img src={graph} />
-      <img src={configure} />
+      <Play status={playStatus} onClick={handlePlayClick} />
+      <Repeat status={repeatStatus} onClick={handleRepeatClick} />
+      <Graph status={graphStatus} onClick={handleGraphStatus} />
+      <Configure status={configureStatus} onClick={handleConfigureStatus} />
     </div>
   );
 }
