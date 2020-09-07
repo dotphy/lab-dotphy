cc5c40abf0408db28e417e5984774f3787324a4a
Aaditay <aaditaygargktl@gmail.com>
2020-09-07 19:24:49 +0530

    :hammer: fixed Controls-Icons rendering

--- src/Components/Controls/Controls.js
+++ src/Components/Controls/Controls.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Controls.css";
-import Pause from "@material-ui/icons/Pause";
+import SvgPlay from "../../Assets/play.js";
+import SvgPause from "../../Assets/pause.js";
+import SvgGraph from "../../Assets/graph.js";
+import SvgRepeat from "../../Assets/repeat.js";
+import SvgConfigure from "../../Assets/configure.js";
 
 function Controls() {
   return <div className="controls"></div>;
