75c0749b35f9550e27a8652aba6852917fc9d594
Aaditay <aaditaygargktl@gmail.com>
2020-10-03 18:01:10 +0530

    ⬆ Lifted the state of Components of Control.js to App.js

--- src/App.js
+++ src/App.js
@@ -4,16 +4,40 @@ import Header from "./Components/Header/Header";
 import Controls from "./Components/Controls/Controls";
 import Experiment from "./Components/Experiment/Experiment";
 
-function App() {
-  return (
-    <div className="app">
-      <Header />
-      <div className="main">
-        <Controls />
-        <Experiment />
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isFullScreenOn: false,
+    };
+
+    //Binding the methods to this"
+    this.handleFullScreenClick = this.handleFullScreenClick.bind(this);
+  }
+  componentDidUpdate() {
+    if (this.state.isFullScreenOn) {
+      document.documentElement.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  }
+  handleFullScreenClick() {
+    this.setState((state) => {
+      return { isFullScreenOn: state.isFullScreenOn ? false : true };
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Header />
+        <div className="main">
+          <Controls handleFullScreenClick={this.handleFullScreenClick} />
+          <Experiment />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
