c9015246adef33651d9033a98a44f8242d6d4fb8
Aaditay <aaditaygargktl@gmail.com>
2020-11-05 17:15:21 +0530

    :lipstick: Progress Bar for Loader

--- src/Components/Loader/Loader.js
+++ src/Components/Loader/Loader.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import LinearProgress from "@material-ui/core/LinearProgress";
 import loader from "../../Assets/loader.png";
 import "./Loader.css";
 
 export default function Loader() {
+  const [loaderValue, setLoaderValue] = useState(100);
+
+  setInterval(() => {
+    setLoaderValue(loaderValue / 4);
+  }, 100);
   return (
     <div className="loader">
       <img src={loader} />
-      <LinearProgress variant="determinate" value={5} />
+      <LinearProgress
+        variant="determinate"
+        value={100 - loaderValue}
+        className="loader__progress"
+      />
     </div>
   );
 }
