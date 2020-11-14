55d2ece1aa0404492c557dc66f31cbab1677aea5
Aaditay <aaditaygargktl@gmail.com>
2020-11-14 17:47:01 +0530

    :lipstick: Header UX

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import dotphy from "../../Assets/dotphy.png";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
@@ -9,6 +9,16 @@ import { Link } from "react-router-dom";
 import { useRouteMatch } from "react-router-dom";
 
 function Header(props) {
+  const [active, setActive] = useState(1);
+
+  function checkRoute() {
+    if (window.location.href.split("/")[3] == "") {
+      setActive(2);
+    } else if (window.location.href.split("/")[3] == "learn") {
+      setActive(1);
+    }
+  }
+
   console.log(window.location.pathname);
   return (
     <div className="header">
@@ -17,12 +27,22 @@ function Header(props) {
       </div>
       <div className="header__middle">
         <Link to="/">
-          <div className="header__option header__option-active">
+          <div
+            className={`header__option ${
+              active == 1 ? "header__option-active" : ""
+            }`}
+            onClick={checkRoute}
+          >
             <Home />
           </div>
         </Link>
         <Link to="/learn">
-          <div className="header__option">
+          <div
+            className={`header__option ${
+              active == 2 ? "header__option-active" : ""
+            }`}
+            onClick={checkRoute}
+          >
             <Book />
           </div>
         </Link>
